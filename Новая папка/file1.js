4f9702ffe73b134e29286629c8429a321118b2e6
sofiosinskaya <72688992+sofiosinskaya@users.noreply.github.com>
2020-10-11 12:56:58 +0300

    Add files via upload

---
+++ Новая папка/file1.js
@@ -0,0 +1,15 @@
+//let dict = {
+//   Имя: '16 лет',
+//   Имя2: '13 лет',
+//  Имя3: '54 года',
+//   123: 321,
+//   911: '1+police',
+//}
+
+let numArr = [10, 9, 1, 2, 99, 101, 66, 33, 28, 54]
+console.log(numArr)
+
+numArr[10] = 15
+console.log(numArr)
+console.log(numArr.length)
+alert(helloworld)
